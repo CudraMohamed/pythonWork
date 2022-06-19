606b4d8e2a0f64deacacdbcfa9fbccca825e9349
CudraMohamed <ncudramohamed@gmail.com>
2022-06-19 13:12:17 +0300

    added dictionaries

--- bank.py
+++ bank.py
@@ -1,4 +1,4 @@
-from unicodedata import name
+from datetime import datetime
 
 class Account:
     def __init__ (self,name,number):
@@ -7,34 +7,52 @@ class Account:
         self.balance= 0
         self.deposits=[]
         self.withdrawals=[]
-
+        self.transaction_cost=100
+        self.now='{:%A-%B-%d %H:%M:%S}'.format(datetime.now())
+        self.withdrawal_dict={}
+        self.deposit_dict={}
 
     def deposit(self,amount):
+        
         if amount<=0:
             return f"deposit amount must be greater than zero"
         else:
          self.balance+=amount
          self.deposits.append(amount)
-        return f"Hello {self.name} you have deposited {amount} your new balance is {self.balance} and your successful deposit amount is {self.deposits}"
+         self.deposit_dict['date']=self.now
+         self.deposit_dict['amount']=amount
+         self.narration= f"Hello {self.name}. Confirmed on {self.now} you deposited {amount} and your new balance is {self.balance}. Your successful deposit amount is {self.deposits}  "
+         self.deposit_dict['narration']=self.narration
+         return self.deposit_dict
 
     
     def withdraw(self,amount):
-        if amount> self.balance:
+        if amount > self.balance:
             return f"Insufficient funds"
         elif amount<=0:
             return f"Amount must be greater than zero"
         else:
             self.withdrawals.append(amount)
-            self.transaction_cost=100
             self.balance-=amount + self.transaction_cost
-            return f"Hello {self.name} you have withdrawn {amount} your new balance is {self.balance} and successful withdrawal amount is{self.withdrawals}"
+            self.withdrawal_dict['date']=self.now
+            self.withdrawal_dict['amount']=amount
+        if amount+self.transaction_cost > self.balance:
+             self.narration= f"Hello {self.name}.Confirmed on {self.now} you have withdrew {amount} and your new balance is {self.balance}. Your successful withdrawal amount is{self.withdrawals}"
+             self.withdrawal_dict['narration']=self.narration
+             return self.withdrawal_dict
+            # withdrawable_balance=self.balance-self.transaction_cost
+            # if amount>withdrawable_balance:
+            #     return "insufficient funds"
 
     def deposits_statement(self):
+       
         for depos in self.deposits:
-            print(f"Your have deposited {depos}")
+            print(f"Your have deposited {depos} on {self.now}")
 
     def withdrawals_statement (self):
+        
         for withdrawn in self.withdrawals:
-            print(f"You have withdrawn: {withdrawn}")
+            print(f"You have withdrawn: {withdrawn} on {self.now}")
     def current_balance(self):
-        return f"Your balance is {self.balance}"
+       
+        return f"Hello {self.name} your current balance is {self.balance} {self.now}"
